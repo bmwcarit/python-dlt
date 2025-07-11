6b3f2b3f1221e76665e57d042008ed8631b9b7c6
Aigars Mahinovs <Aigars.Mahinovs@bmw.de>
2025-07-11 14:55:27 +0200

    Bump software status from internal repo
    * support 128bit data fields
    * add unit tests
    * add basic Sphinx docs
    * 2.18.5 did not actually work anymore

---
+++ docs/conf.py
@@ -0,0 +1,58 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+import datetime
+
+# -- Project information -----------------------------------------------------
+
+project = "dlt"
+copyright = "%(year)s, %(author)s" % dict(year=datetime.date.today().year, author="BMW Car IT")
+author = "BMW Car IT"
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosectionlabel", "sphinx_click.ext", "sphinx_multiversion"]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+# sphinx-multiversion configuration
+# Format for versioned output directories inside the build directory
+smv_outputdir_format = "{ref.name}"
+# Include only master branch
+smv_branch_whitelist = r"^master.*$"
+# Determines whether remote or local git branches/tags are preferred if their output dirs conflict
+smv_prefer_remote_refs = False
