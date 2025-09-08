6333b2d55019a6cb10054c416c07c0cc7c2ee296
Wen-Shih Chao <Wen-Shih.Chao@bmw.de>
2025-09-08 09:56:41 +0200

    fix: refine format by black

--- tests/dlt_client_unit_test.py
+++ tests/dlt_client_unit_test.py
@@ -17,13 +17,15 @@ class TestDLTClient(unittest.TestCase):
         self.assertFalse(self.client.connect(timeout=2))
 
         # - dlt_receiver_init error
-        with patch("socket.create_connection", return_value=Mock(fileno=Mock(return_value=2000000))), patch(
-            "dlt.dlt.dltlib.dlt_receiver_init", return_value=DLT_RETURN_ERROR
+        with (
+            patch("socket.create_connection", return_value=Mock(fileno=Mock(return_value=2000000))),
+            patch("dlt.dlt.dltlib.dlt_receiver_init", return_value=DLT_RETURN_ERROR),
         ):
             self.assertFalse(self.client.connect(timeout=2))
 
     def test_connect_with_timeout_success(self):
-        with patch("socket.create_connection", return_value=Mock(fileno=Mock(return_value=2000000))), patch(
-            "dlt.dlt.dltlib.dlt_receiver_init", return_value=DLT_RETURN_OK
+        with (
+            patch("socket.create_connection", return_value=Mock(fileno=Mock(return_value=2000000))),
+            patch("dlt.dlt.dltlib.dlt_receiver_init", return_value=DLT_RETURN_OK),
         ):
             self.assertTrue(self.client.connect(timeout=2))
